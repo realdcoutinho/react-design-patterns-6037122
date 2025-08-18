e067b4818f3e021f9ad6fbde5dde28ed9acf0fa7
realdcoutinho <coutinho.dev@yahoo.com>
2025-08-18 09:53:12 +0100

    Recursive Components

--- functional-react/src/App.jsx
+++ functional-react/src/App.jsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { RecursiveComponent } from "./RecursiveComponent"
+
+const nestedObject = 
+{
+  a: 1,
+  b: {
+    b1: 2,
+    b2: {
+      b23: 'Hello',
+    },
+    b3: {
+      b31: {
+        message: 'Hi',
+      },
+      b32: {
+        message: 'Hi',
+      }
+    }
+  },
+  c: {
+    c1: 2,
+    c2: 3,
+  }
+}
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <RecursiveComponent data={nestedObject} />
   )
 }
 
---
+++ functional-react/src/RecursiveComponent.jsx
@@ -0,0 +1,26 @@
+const isObject = x => typeof x === 'object' && x !== null;
+
+export function RecursiveComponent({data})
+{
+  if(!isObject(data))
+  {
+    return <li>{data}</li>
+  }
+
+  const pairs = Object.entries(data);
+
+  return (
+    <>
+    
+      {pairs.map(([key, value]) => (
+          <li>
+            {key}:
+            <ul>
+              <RecursiveComponent data={value} />
+            </ul>
+          </li>
+      ))}
+    </>
+  )
+
+}
